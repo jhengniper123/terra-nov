a8779bba8c1f996e2e8ad8735f8111669a55156d
jheng <jhengniper@gmail.com>
2025-11-21 11:27:12 +0800

    make resources

--- resource.tf
+++ resource.tf
@@ -1,3 +1,23 @@
+# resource "azurerm_resource_group" "rg1" {
+#   name     = "jheng-rg"
+#   location = "eastus"
+# }
+
+# resource "azurerm_virtual_network" "linux-vnet" {
+#   name                = "linux-network"
+#   location            = azurerm_resource_group.rg1.location
+#   resource_group_name = azurerm_resource_group.rg1.name
+#   address_space       = ["10.0.0.0/16"]
+#   dns_servers         = ["10.0.0.4", "10.0.0.5", "8.8.8.8"]
+
+# }
+
+# resource "azurerm_subnet" "linux-subnet" {
+#   name                 = "linux-subnet"
+#   resource_group_name  = azurerm_resource_group.rg1.name
+#   virtual_network_name = azurerm_virtual_network.linux-vnet.name
+#   address_prefixes     = ["10.0.20.0/24"]
+# }
 resource "azurerm_resource_group" "rg1" {
   name     = "jheng-rg"
   location = "eastus"
@@ -17,4 +37,14 @@ resource "azurerm_subnet" "linux-subnet" {
   resource_group_name  = azurerm_resource_group.rg1.name
   virtual_network_name = azurerm_virtual_network.linux-vnet.name
   address_prefixes     = ["10.0.20.0/24"]
+}
+
+resource "azurerm_public_ip" "linux-pip" {
+  name                = "linux-pip"
+  resource_group_name = azurerm_resource_group.rg1.name
+  location            = azurerm_resource_group.rg1.location
+  allocation_method   = "Static"
+  lifecycle {
+    create_before_destroy = true
+  }
 }
